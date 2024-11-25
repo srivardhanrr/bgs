28745529f5f411d218a6eeb1363ab2525037a3be
srivardhanrr <srivardhan.singh.rathore@gmail.com>
2024-11-25 11:20:42 +0530

    first

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,7 @@
 
     let {children} = $props();
     import {onMount} from 'svelte';
-    import {gsap} from 'gsap';
-    import {ScrollTrigger} from 'gsap/ScrollTrigger';
     import Header from "$lib/components/Header.svelte";
-
-    gsap.registerPlugin(ScrollTrigger);
 
     onMount(() => {
         // Initialize any global animations here
